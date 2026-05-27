74cc774a2143b67ff1083ca9bdabe27144298baf
Maria Nadal <marianadal.mnm@gmail.com>
2026-05-27 14:32:29 +0200

    commit 1234567890abcdef1234567890abcdef12345678

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression' // 🔥 1. Importamos el plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 🔥 2. Lo añadimos a la lista de plugins
+    viteCompression({
+      algorithm: 'gzip', // Comprime usando el algoritmo estándar que leen todos los móviles
+      ext: '.gz',        // Añade la extensión .gz a los archivos comprimidos
+    })
+  ],
 })
